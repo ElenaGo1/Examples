cded44a61806b26712b791b20f1de7467d7ec7c5
Lena <110883931+ElenaGo1@users.noreply.github.com>
2022-09-17 12:31:32 +0300

    Example016_recursion_factorial

---
+++ Example016_recursion_factorial/Program.cs
@@ -0,0 +1,33 @@
+﻿//Рекурсия функций нв примере решения факториала
+/*
+int Factorial(int n)
+{
+    //1! = 1
+    //0! = 1 
+    if (n==1) return 1;
+    else return n * Factorial(n-1);
+}
+//Console.WriteLine(Factorial(3));//1*2*3 = 6
+for (int i = 1; i < 40; i++)
+{
+    //Console.WriteLine(Factorial(i));//1*2*3 = 6
+    Console.WriteLine($"{i} = {Factorial(i)}");
+}
+*/
+
+// ========
+//f(1) = 1
+//f(2) = 1
+//f(n) = f(n-1) + f(n-2)
+/*
+double Fibonacci (int n)
+{ 
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2); 
+}
+for (int i = 1; i < 47; i++)
+{
+    //Console.WriteLine(Factorial(i));//1*2*3 = 6
+    Console.WriteLine($"{i} = {Fibonacci(i)}");
+}
+*/
